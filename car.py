14cdec728d4522df5946378bb56fbf4303f64ec4
IvánLevente <ivan.levente@diak.szbi-pg.hu>
2026-03-04 10:18:18 +0100

    Update car.py

--- car.py
+++ car.py
@@ -1,12 +1,15 @@
 class Auto:
-    def __init__(self, marka_, tipus_, gyartasi_ev_):
+    def __init__(self, marka_, tipus_, gyartasi_ev_ ,fogyasztas_=7.5, uzemanyag_=50):
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
         self.sebesseg = 0
+        self.fogyasztas = fogyasztas_
+        self.uzemanyag = uzemanyag_
 
     def __str__(self):
-        return f"{self.marka} {self.tipus} ({self.gyartasi_ev}), sebesség: {self.sebesseg} km/h"
+        return f"{self.marka} {self.tipus} ({self.gyartasi_ev}),\
+        sebesség: {self.sebesseg} km/h, üzemanyag {self.uzemanyag} l"
 
     def gyorsit(self, ertek):
         self.sebesseg += ertek
@@ -19,3 +22,17 @@ class Auto:
         if self.sebesseg < 0:
             self.sebesseg = 0
             print("Megálltál!")
+
+    def tankol(self, mennyiseg):
+        self.uzemanyag += mennyiseg
+        if self.uzemanyag > 50:
+            self.uzemanyag = 50
+            print("Tank tele!")
+
+    def utazik(self, tavolsag):
+        fogyasztott_uzemanyag = (tavolsag / 100) * self.fogyasztas
+        if fogyasztott_uzemanyag > self.uzemanyag:
+            print("Tankolni kell!")
+        else:
+            self.uzemanyag -= fogyasztott_uzemanyag
+            print(f"Fogyasztott üzemanyag: {fogyasztott_uzemanyag:.2f} liter")
